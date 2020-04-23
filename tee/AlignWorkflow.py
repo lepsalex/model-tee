6eaef619cc81bb547fb100c749cd0db128839ccc
Alex <lepsalex@gmail.com>
2020-04-23 13:05:09 -0400

    integrated report.py into normal flow

--- tee/AlignWorkflow.py
+++ tee/AlignWorkflow.py
@@ -17,22 +17,36 @@ class AlignWorkflow(WorkflowBase):
             "params": data["request"]["workflow_params"],
             "start": data["run_log"]["start_time"],
             "end": data["run_log"]["end_time"],
-            "duration": data["run_log"]["duration"],
+            "duration": round(data["run_log"]["duration"] / 1000 / 60 / 60, 2),
             "tasks": list(filter(None, map(self.processTasks, data["task_logs"])))
         }
 
     def mergeRunsWithSheetData(self, runs):
         # take only the latest entry per analysis_id (data is sorted by date at server)
         latest_runs = runs.sort_values(["start"], ascending=False).groupby("analysis_id").head(1)
-
+        
         # Update sheet data
-        new_sheet_data = pd.merge(self.sheet_data, latest_runs[["analysis_id", "run_id", "state"]], on="analysis_id", how="left")
+        new_sheet_data = pd.merge(self.sheet_data, latest_runs[["analysis_id", "run_id", "state", "start", "end", "duration"]], on="analysis_id", how="left")      
         new_sheet_data["run_id"] = new_sheet_data["run_id_y"].fillna(new_sheet_data["run_id_x"])
         new_sheet_data["state"] = new_sheet_data["state_y"].fillna(new_sheet_data["state_x"])
+        new_sheet_data["start"] = new_sheet_data["start_y"].fillna(new_sheet_data["start_x"])
+        new_sheet_data["end"] = new_sheet_data["end_y"].fillna(new_sheet_data["end_x"])
+        new_sheet_data["duration"] = new_sheet_data["duration_y"].fillna(new_sheet_data["duration_x"])
 
-        return new_sheet_data.drop(["state_y", "state_x", "run_id_x", "run_id_y"], axis=1)
+        return new_sheet_data.drop([
+            "run_id_x",
+            "run_id_y",
+            "state_y",
+            "state_x",
+            "start_x",
+            "start_y",
+            "end_x",
+            "end_y",
+            "duration_x",
+            "duration_y",
+        ], axis=1)
 
-    def buildRunRequests(self, run, resume = False):
+    def buildRunRequests(self, run, resume=False):
         config = {
             "study_id": run["study_id"],
             "analysis_id": run["analysis_id"],
