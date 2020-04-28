439e96f5d59dc572ecff83db3f51208c4c3b6d9d
Alex <lepsalex@gmail.com>
2020-04-28 16:38:44 -0400

    SWXS request

--- tee/model/SangerWXSRequest.py
+++ tee/model/SangerWXSRequest.py
@@ -7,69 +7,45 @@ class SangerWXSRequest(WorkflowRequestBase):
 
     def buildWorkflowParams(self, run_config, song_score_config):
         study_id = run_config["study_id"]
-        analysis_id = run_config["analysis_id"]
+        normal_aln_analysis_id = run_config["normal_aln_analysis_id"]
+        tumour_aln_analysis_id = run_config["tumour_aln_analysis_id"]
         work_dir = run_config["work_dir"]
-        cpus = run_config["max_cpus"]
-        mem = max((cpus * 3) + 2, run_config["min_mem"])
+        cpus = run_config["cpus"]
+        mem = run_config["mem"]
 
         params = {
             "study_id": study_id,
-            "analysis_id": analysis_id,
+            "normal_aln_analysis_id": normal_aln_analysis_id,
+            "tumour_aln_analysis_id": tumour_aln_analysis_id,
             "song_url": song_score_config["SONG_URL"],
             "score_url": song_score_config["SCORE_URL"],
             "api_token": song_score_config["SONG_API_TOKEN"],
-            "ref_genome_fa": "/{}/reference/GRCh38_hla_decoy_ebv/GRCh38_hla_decoy_ebv.fa".format(work_dir),
-            "cpus": 2,
-            "mem": 4,
+            "cpus": 1,
+            "mem": 1,
             "download": {
                 "song_cpus": 2,
                 "song_mem": 2,
                 "score_cpus": 8,
                 "score_mem": 18
             },
-            "seqDataToLaneBam": {
+            "sangerWxsVariantCaller": {
                 "cpus": cpus,
-                "mem": mem
+                "mem": mem,
+                "exclude": "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr22,chrX,chrY,chrUn%,HLA%,%_alt,%_random,chrM,chrEBV",
+                "vagrent_annot": "/{}/reference/sanger-variant-calling/VAGrENT_ref_GRCh38_hla_decoy_ebv_ensembl_91.tar.gz".format(work_dir),
+                "ref_genome_tar": "/{}/reference/sanger-variant-calling/core_ref_GRCh38_hla_decoy_ebv.tar.gz".format(work_dir),
+                "ref_snv_indel_tar": "/{}/reference/sanger-variant-calling/SNV_INDEL_ref_GRCh38_hla_decoy_ebv-fragment.tar.gz".format(work_dir),
             },
-            "bwaMemAligner": {
-                "cpus": cpus,
-                "mem": mem
+            "generateBas": {
+                "ref_genome_fa": "/{}/reference/GRCh38_hla_decoy_ebv/GRCh38_hla_decoy_ebv.fa".format(work_dir)
             },
-            "bamMergeSortMarkdup": {
-                "cpus": 4,
-                "mem": 18
+            "repackSangerResults": {
+                "cpus": 1,
+                "mem": 1
             },
-            "payloadGenDnaAlignment": {
-                "cpus": 2,
-                "mem": 4
-            },
-            "readGroupUBamQC": {
-                "cpus": 3,
-                "mem": 6
-            },
-            "alignedSeqQC": {
-                "cpus": 4,
-                "mem": 10
-            },
-            "gatkCollectOxogMetrics": {
-                "cpus": 3,
-                "mem": 6
-            },
-            "payloadGenDnaSeqQc": {
-                "cpus": 2,
-                "mem": 2
-            },
-            "uploadAlignment": {
-                "song_cpus": 2,
-                "song_mem": 2,
-                "score_cpus": 8,
-                "score_mem": 18
-            },
-            "uploadQc": {
-                "song_cpus": 2,
-                "song_mem": 2,
-                "score_cpus": 2,
-                "score_mem": 4
+            "repack-sanger-results": {
+                "cpus": 1,
+                "mem": 1
             },
             "cleanup": True
         }
@@ -83,6 +59,6 @@ class SangerWXSRequest(WorkflowRequestBase):
 
     def __str__(self):
         """
-        Represent and request against analysis_id
+        Represent and request against combo of normal_aln_analysis_id and tumour_aln_analysis_id
         """
-        return self.wp_config["analysis_id"]
+        return "normal: {} - tumor: {}".format(self.wp_config["normal_aln_analysis_id"], self.wp_config["tumour_aln_analysis_id"])
