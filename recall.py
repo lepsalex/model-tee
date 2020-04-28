901770c4668b59a45f8eb775a5971ebdd0d386e9
Alex <lepsalex@gmail.com>
2020-04-28 11:28:33 -0400

    setting path for env

--- recall.py
+++ recall.py
@@ -3,7 +3,7 @@ from tee.AlignWorkflow import AlignWorkflow
 from dotenv import load_dotenv
 
 # load env from file if present
-load_dotenv()
+load_dotenv(".env")
 
 # Build workflow objects
 align_workflow = AlignWorkflow({
