9ad188ee0d2c36eb3ff30edcbf303884abc1f996
Alex <lepsalex@gmail.com>
2020-06-02 16:55:03 -0400

    make Kafka consumer more generic

--- service/Kafka.py
+++ service/Kafka.py
@@ -11,14 +11,14 @@ class Kafka:
     TOPIC = os.getenv("KAFKA_TOPIC", "workflow")
 
     @classmethod
-    def consumeTopicWith(cls, onMessageFunc):
+    def consumeTopicWith(cls, topic, onMessageFunc):
         print("Starting Kafka consumer ...")
         consumer = KafkaConsumer(client_id="model-tee",
                                  group_id="model-tee-workflow-consumer",
                                  bootstrap_servers=cls.BOOTSTRAP_SERVERS,
                                  value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-        consumer.subscribe([cls.TOPIC])
+        consumer.subscribe(topic)
 
         for message in consumer:
             onMessageFunc(message)
