334a72e7cae9b981d8465bf7c94a444b49a22cba
Alex <lepsalex@gmail.com>
2020-07-02 15:54:28 -0400

    swapping recall to align

--- recall.py
+++ recall.py
@@ -7,33 +7,33 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build workflow objects
-# align_workflow = AlignWorkflow({
-#     "sheet_id": os.getenv("ALIGN_SHEET_ID"),
-#     "sheet_range": os.getenv("ALIGN_SHEET_RANGE"),
-#     "wf_url": os.getenv("ALIGN_WF_URL"),
-#     "wf_version": os.getenv("ALIGN_WF_VERSION"),
-#     "max_runs": os.getenv("ALIGN_MAX_RUNS"),
-#     "max_runs_per_dir": os.getenv("ALIGN_MAX_RUNS_PER_DIR"),
-#     "cpus": os.getenv("ALIGN_CPUS"),
-#     "mem": os.getenv("ALIGN_MEM")
-# })
-
-sanger_wgs_workflow = SangerWGSWorkflow({
-    "sheet_id": os.getenv("SANGER_WGS_SHEET_ID"),
-    "sheet_range": os.getenv("SANGER_WGS_SHEET_RANGE"),
-    "wf_url": os.getenv("SANGER_WGS_WF_URL"),
-    "wf_version": os.getenv("SANGER_WGS_WF_VERSION"),
-    "max_runs": os.getenv("SANGER_WGS_MAX_RUNS"),
-    "max_runs_per_dir": os.getenv("SANGER_WGS_MAX_RUNS_PER_DIR"),
-    "cpus": os.getenv("SANGER_WGS_CPUS"),
-    "mem": os.getenv("SANGER_WGS_MEM")
+align_workflow = AlignWorkflow({
+    "sheet_id": os.getenv("ALIGN_SHEET_ID"),
+    "sheet_range": os.getenv("ALIGN_SHEET_RANGE"),
+    "wf_url": os.getenv("ALIGN_WF_URL"),
+    "wf_version": os.getenv("ALIGN_WF_VERSION"),
+    "max_runs": os.getenv("ALIGN_MAX_RUNS"),
+    "max_runs_per_dir": os.getenv("ALIGN_MAX_RUNS_PER_DIR"),
+    "cpus": os.getenv("ALIGN_CPUS"),
+    "mem": os.getenv("ALIGN_MEM")
 })
+
+# sanger_wgs_workflow = SangerWGSWorkflow({
+#     "sheet_id": os.getenv("SANGER_WGS_SHEET_ID"),
+#     "sheet_range": os.getenv("SANGER_WGS_SHEET_RANGE"),
+#     "wf_url": os.getenv("SANGER_WGS_WF_URL"),
+#     "wf_version": os.getenv("SANGER_WGS_WF_VERSION"),
+#     "max_runs": os.getenv("SANGER_WGS_MAX_RUNS"),
+#     "max_runs_per_dir": os.getenv("SANGER_WGS_MAX_RUNS_PER_DIR"),
+#     "cpus": os.getenv("SANGER_WGS_CPUS"),
+#     "mem": os.getenv("SANGER_WGS_MEM")
+# })
 
 # Recall Script (to be run locally only!)
 recall_list = []
 
-# align_workflow.update()
+align_workflow.update()
 # align_workflow.recall(recall_list)
 
-sanger_wgs_workflow.update()
+# sanger_wgs_workflow.update()
 # sanger_wgs_workflow.recall(recall_list)
