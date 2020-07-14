db00fb1d8dbaffe5ea807168475d22409ddd0435
Alex <lepsalex@gmail.com>
2020-07-14 16:43:51 -0400

    recall to WXS mode

--- recall.py
+++ recall.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build workflow objects
-align_wgs_workflow = AlignWorkflow({
-    "sheet_id": os.getenv("ALIGN_WGS_SHEET_ID"),
-    "sheet_range": os.getenv("ALIGN_WGS_SHEET_RANGE"),
-    "wf_url": os.getenv("ALIGN_WGS_WF_URL"),
-    "wf_version": os.getenv("ALIGN_WGS_WF_VERSION"),
-    "max_runs": os.getenv("ALIGN_WGS_MAX_RUNS"),
-    "max_runs_per_dir": os.getenv("ALIGN_WGS_MAX_RUNS_PER_DIR"),
-    "cpus": os.getenv("ALIGN_WGS_CPUS"),
-    "mem": os.getenv("ALIGN_WGS_MEM")
-})
+# align_wgs_workflow = AlignWorkflow({
+#     "sheet_id": os.getenv("ALIGN_WGS_SHEET_ID"),
+#     "sheet_range": os.getenv("ALIGN_WGS_SHEET_RANGE"),
+#     "wf_url": os.getenv("ALIGN_WGS_WF_URL"),
+#     "wf_version": os.getenv("ALIGN_WGS_WF_VERSION"),
+#     "max_runs": os.getenv("ALIGN_WGS_MAX_RUNS"),
+#     "max_runs_per_dir": os.getenv("ALIGN_WGS_MAX_RUNS_PER_DIR"),
+#     "cpus": os.getenv("ALIGN_WGS_CPUS"),
+#     "mem": os.getenv("ALIGN_WGS_MEM")
+# })
 
 align_wxs_workflow = AlignWorkflow({
     "sheet_id": os.getenv("ALIGN_WXS_SHEET_ID"),
@@ -44,10 +44,10 @@ align_wxs_workflow = AlignWorkflow({
 # Recall Script (to be run locally only!)
 recall_list = []
 
-align_wgs_workflow.update()
+# align_wgs_workflow.update()
 # align_wgs_workflow.recall(recall_list)
 
-# align_wxs_workflow.update()
+align_wxs_workflow.update()
 # align_wxs_workflow.recall(recall_list)
 
 # sanger_wgs_workflow.update()
