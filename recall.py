3f1a8d46c64c3e7dac2ee7edae38fab0ff63aa37
Alex <lepsalex@gmail.com>
2020-07-16 12:38:16 -0400

    add SangeWXS to recall

--- recall.py
+++ recall.py
@@ -1,6 +1,7 @@
 import os
 from tee.AlignWorkflow import AlignWorkflow
 from tee.SangerWGSWorkflow import SangerWGSWorkflow
+from tee.SangerWXSWorkflow import SangerWXSWorkflow
 from dotenv import load_dotenv
 
 # load env from file if present
@@ -12,7 +13,7 @@ align_wgs_workflow = AlignWorkflow({
     "sheet_range": os.getenv("ALIGN_WGS_SHEET_RANGE"),
     "wf_url": os.getenv("ALIGN_WGS_WF_URL"),
     "wf_version": os.getenv("ALIGN_WGS_WF_VERSION"),
-    "max_runs": -1,
+    "max_runs": 0,
     "max_runs_per_dir": -1,
     "cpus": os.getenv("ALIGN_WGS_CPUS"),
     "mem": os.getenv("ALIGN_WGS_MEM")
@@ -41,10 +42,21 @@ sanger_wgs_workflow = SangerWGSWorkflow({
     "mem": os.getenv("SANGER_WGS_MEM")
 })
 
+sanger_wxs_workflow = SangerWXSWorkflow({
+    "sheet_id": os.getenv("SANGER_WXS_SHEET_ID"),
+    "sheet_range": os.getenv("SANGER_WXS_SHEET_RANGE"),
+    "wf_url": os.getenv("SANGER_WXS_WF_URL"),
+    "wf_version": os.getenv("SANGER_WXS_WF_VERSION"),
+    "max_runs": -1,
+    "max_runs_per_dir": -1,
+    "cpus": os.getenv("SANGER_WXS_CPUS"),
+    "mem": os.getenv("SANGER_WXS_MEM")
+})
+
 # Recall Script (to be run locally only!)
 recall_list = []
 
-align_wgs_workflow.update()
+# align_wgs_workflow.update()
 # align_wgs_workflow.recall(recall_list)
 
 # align_wxs_workflow.update()
@@ -52,3 +64,6 @@ align_wgs_workflow.update()
 
 # sanger_wgs_workflow.update()
 # sanger_wgs_workflow.recall(recall_list)
+
+sanger_wxs_workflow.update()
+# sanger_wxs_workflow.recall(recall_list)
