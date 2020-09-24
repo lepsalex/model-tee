3ea94344088c92626bd7e93bc8cf30babbea6bc9
Alex <lepsalex@gmail.com>
2020-09-24 13:36:41 -0400

    projects are now versioned at the folder level to avoid nextflow bugs

--- tee/model/WorkflowRequestBase.py
+++ tee/model/WorkflowRequestBase.py
@@ -41,11 +41,11 @@ class WorkflowRequestBase(ABC):
 
         engine_params = {}
 
-        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "launch_dir", "/{}/launch", run_config.get("work_dir", None))
-        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "project_dir", "/{}/project", run_config.get("work_dir", None))
-        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "work_dir", "/{}/work", run_config.get("work_dir", None))
+        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "launch_dir", "/%s/launch", run_config.get("work_dir", None))
+        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "project_dir", "/%s/versioned-projects/%s/", run_config.get("work_dir", None), run_config.get("revision", "master"))
+        WorkflowRequestBase.addFormattedStringValueIfValue(engine_params, "work_dir", "/%s/work", run_config.get("work_dir", None))
 
-        WorkflowRequestBase.addValueIfValue(engine_params, "revision", run_config.get("revision", None))
+        WorkflowRequestBase.addValueIfValue(engine_params, "revision", run_config.get("revision", "master"))
         WorkflowRequestBase.addValueIfValue(engine_params, "resume", run_config.get("resume", None))
 
         return engine_params
@@ -58,8 +58,8 @@ class WorkflowRequestBase(ABC):
         dict[key] = val
 
     @classmethod
-    def addFormattedStringValueIfValue(cls, dict, key, formatted, val):
-        if not val:
+    def addFormattedStringValueIfValue(cls, dict, key, formatted, *val):
+        if None in val:
             return
 
-        dict[key] = formatted.format(val)
+        dict[key] = formatted % tuple(val)
