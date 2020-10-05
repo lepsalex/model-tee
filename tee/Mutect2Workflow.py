e211c0a044235420952559ee569967b750602710
Alex <lepsalex@gmail.com>
2020-10-05 10:28:19 -0400

    mutect2 is really a variant caller

--- tee/Mutect2Workflow.py
+++ tee/Mutect2Workflow.py
@@ -1,77 +1,25 @@
 import pandas as pd
-from tee.WorkflowBase import WorkflowBase
+from tee.SangerWorkflowBase import SangerWorkflowBase
 from tee.model.Mutect2Request import Mutect2Request
 
 
-class Mutect2Workflow(WorkflowBase):
+class Mutect2Workflow(SangerWorkflowBase):
 
     def __init__(self, config):
         super().__init__(config)
-        self.index_cols = ["analysis_id"]
-
-    def transformRunData(self, data):
-        return {
-            "analysis_id": data["parameters"]["analysis_id"],
-            "work_dir": data["engineParameters"]["workDir"].split("/")[1],
-            "run_id": data["runId"],
-            "session_id": data["sessionId"],
-            "state": data["state"],
-            "params": data["parameters"],
-            "start": self.esTimestampToLocalDate(data["startTime"]),
-            "end": self.esTimestampToLocalDate(data["completeTime"]),
-            "duration": float(round(int(data["duration"]) / 1000 / 60 / 60, 2)) if data["duration"] and data["duration"] != 0 else None,
-        }
-
-    def mergeRunsWithSheetData(self, runs):
-        # take only the latest entry per analysis_id (data is sorted by date at server)
-        latest_runs = runs.sort_values(["start"], ascending=False).groupby("analysis_id").head(1)
-
-        # Update sheet data
-        new_sheet_data = pd.merge(self.sheet_data, latest_runs[["analysis_id", "work_dir", "run_id", "session_id",
-                                                                "state", "start", "end", "duration"]], on="analysis_id", how="left")
-
-        new_sheet_data["work_dir"] = new_sheet_data["work_dir_y"].fillna("")
-        new_sheet_data["run_id"] = new_sheet_data["run_id_y"].fillna("")
-        new_sheet_data["session_id"] = new_sheet_data["session_id_y"].fillna("")
-        new_sheet_data["state"] = new_sheet_data["state_y"].fillna("")
-        new_sheet_data["start"] = new_sheet_data["start_y"].fillna("")
-        new_sheet_data["end"] = new_sheet_data["end_y"].fillna("")
-        new_sheet_data["duration"] = new_sheet_data["duration_y"].fillna("")
-
-        return new_sheet_data.drop([
-            "work_dir_x",
-            "work_dir_y",
-            "run_id_x",
-            "run_id_y",
-            "session_id_x",
-            "session_id_y",
-            "state_y",
-            "state_x",
-            "start_x",
-            "start_y",
-            "end_x",
-            "end_y",
-            "duration_x",
-            "duration_y",
-        ], axis=1)
 
     def buildRunRequests(self, run, resume=False):
         config = {
             "study_id": run["study_id"],
-            "analysis_id": run["analysis_id"],
+            "normal_aln_analysis_id": run["normal_aln_analysis_id"],
+            "tumour_aln_analysis_id": run["tumour_aln_analysis_id"],
             "work_dir": run["work_dir"],
             "revision": self.wf_version,
             "cpus": int(self.cpus),
-            "mem": int(self.mem)
+            "mem": int(self.mem),
         }
 
         if resume:
             config["resume"] = run["session_id"]
 
-        return Mutect2Request(self.wf_url, config)
-
-    def transformEventData(self, event_data):
-        return {
-            "study_id": event_data["study"],
-            "analysis_id": event_data["analysisId"],
-        }
+        return Mutect2Workflow(self.wf_url, config)
